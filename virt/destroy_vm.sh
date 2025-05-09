402637383f573a0ed517012b4dc99d20ca8dd43e
mood <74420740+no-mood@users.noreply.github.com>
2025-05-09 19:37:06 +0200

    Update image path handling in create_vm.sh and destroy_vm.sh for consistency

--- virt/destroy_vm.sh
+++ virt/destroy_vm.sh
@@ -2,6 +2,7 @@
 
 # Variables
 VM_NAME="ubuntu-xdp-24.04"
+DISK_PATH="./noble-server-cloudimg-amd64.img"  # Aggiornato per usare il percorso corrente
 
 # Check if the VM exists
 if ! virsh --connect qemu:///system dominfo "$VM_NAME" &>/dev/null; then
@@ -18,13 +19,12 @@ echo "Undefining VM '$VM_NAME'..."
 virsh --connect qemu:///system undefine "$VM_NAME"
 
 # Optionally, remove the disk image
-DISK_PATH="/var/lib/libvirt/images/noble-server-cloudimg-amd64.img"
 if [ -f "$DISK_PATH" ]; then
   read -p "Do you want to remove the disk image at '$DISK_PATH'? [y/N]: " CONFIRM
   CONFIRM=${CONFIRM:-n}  # Default to "no" if the user presses Enter
   if [[ "$CONFIRM" =~ ^[Yy]$ ]]; then
     echo "Removing disk image at '$DISK_PATH'..."
-    sudo rm -f "$DISK_PATH"
+    rm -f "$DISK_PATH"
   else
     echo "Disk image not removed."
   fi
